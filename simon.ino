0f80fda347ffe07d31997a9fcb2ffe3446180049
Roman Berger <romanberger001@gmail.com>
2023-02-12 16:02:27 +0000

    removed an include

--- simon.ino
+++ simon.ino
@@ -1,16 +1,18 @@
-#include "Simon.hpp"
-
 #include <Beginner_LED.h>
 
 #include "Controller.h"
 
+Controller c1;
+
 void setup(){
     // do stuff
 
-  Serial.begin(9600);
+    Serial.begin(9600);
+    c1.setup();
 }
 
 void loop(){
+  //Serial.println("1");
     // do stuff
-
+    //c1.play();
 }
