ab8780174a3324a6cbdc9a9e8d696d737402fc4d
Roman Berger <romanberger001@gmail.com>
2023-02-15 21:02:41 +0000

    can read a sequence and fixed how the components are added

--- InputHandler.h
+++ InputHandler.h
@@ -6,10 +6,18 @@
 
 class InputHandler{
   Array<IInput*, 10> m_ins; 
+
+  int input_time;
 public:
   InputHandler(){
     // initialize the serial line for debugging
     Serial.begin(9600);
+    input_time = 30*1000;
+
+    // register the input devices
+    m_ins.push_back( new Button((uint8_t)2));
+    m_ins.push_back( new Button((uint8_t)4));
+    m_ins.push_back( new Button((uint8_t)7));
   }
 
   void test(){
@@ -31,16 +39,29 @@ public:
 
   void setup(){
     // add two buttons as input devices
-    Button b1((uint8_t)2);
-    m_ins.push_back(*b1);
-
-    Button b2((uint8_t)4);
-    m_ins.push_back(*b2);
-
     test();
   }
 
-  void read_sequence(){
+  void read_sequence(int* read_seqi, int difficulty){
+    long double timer = millis();
+    int i = 0;
+    bool read;
+
+    Serial.println("entered");
+
+    while(millis() <= timer + input_time && i < difficulty){
+      // do the reading in the permitted window
+      for(IInput* in : m_ins){
+        read = in->read();
+        if(read){
+          in->get_value();
+          i++;
+          read = false;
+          break;
+        }
+      }
+      Serial.println(i);
+    }
     // do nothing for now
   }
 };
