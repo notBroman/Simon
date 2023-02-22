ccbe7791390b609839c636499d83a711dbd16728
Roman Berger <romanberger001@gmail.com>
2023-02-22 20:00:13 +0000

    comment

--- simon.ino
+++ simon.ino
@@ -4,13 +4,11 @@ Controller c1;
 
 void setup(){
     // do stuff
-
-    Serial.begin(9600);
+    Serial.begin(9600); // for debugging
     c1.setup();
 }
 
 void loop(){
-
   // do stuff
   c1.play();
 }
