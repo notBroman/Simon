a89538b33055a62be507339419c7ec49b9031796
Roman Berger <romanberger001@gmail.com>
2023-02-24 11:12:22 +0000

    using new constructors

--- simon.ino
+++ simon.ino
@@ -1,14 +1,31 @@
 #include "Controller.h"
 
-Controller c1;
+int i1[3] = {2,4,7};
+int i2[3] = {8,12,13};
+
+int o1[3] = {3,5,6};
+int o2[3] = {9,10,11};
+
+Controller* c1;
+Controller* c2;
 
 void setup(){
     // do stuff
-    Serial.begin(9600); // for debugging
-    c1.setup();
+    //Serial.begin(9600); // for debugging
+    while(!Serial){}
+
+    //Serial.println("hello world");
+
+    c1 = new Controller(i1, 3, o1, 3);
+    c2 = new Controller(i2, 3, o2, 3);
+
+    c1->setup();
+    c2->setup();
+
 }
 
 void loop(){
   // do stuff
-  c1.play();
+  c1->play();
+  c2->play();
 }
